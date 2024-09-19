73f6c5be1b8aa8b7582f4e6c0b83f353fb51e2ee
Michael Vangelovski <michael.vangelovski@hotmail.com>
2024-09-19 11:48:11 +1000

    Create multi-day schedules

--- classes/task/cron_create_classes.php
+++ classes/task/cron_create_classes.php
@@ -83,76 +83,131 @@ class cron_create_classes extends \core\task\scheduled_task {
                 $activitystart = date('Y-m-d H:i', $activity->get('timestart'));
                 $activityend = date('Y-m-d H:i', $activity->get('timeend'));
 
-                // 1. Create the class.
-                $this->log("Creating the class " . 'EX_' . $activity->get('id') . ", with staff in charge " .  $activity->get('staffincharge') . ", start time " .  $activitystart, 2 );
-                $sql = $config->createclasssql . ' :fileyear, :filesemester, :classcampus, :classcode, :description, :staffid, :leavingdate, :returningdate';
-                
-                // Keep within schedule limits.
-                $activitystarthour = date('H', $activity->get('timestart'));
-                if ($activitystarthour < 6) {
-                    $activitystart = date('Y-m-d 06:i', $activity->get('timestart'));
-                }
-                if ($activitystarthour > 18 ) {
-                    $activitystart = date('Y-m-d 18:i', $activity->get('timestart'));
+                // If this activity is multiple days, break it into days and create a class for each day so that roll marking works.
+                // Convert start and end times to DateTime objects
+                $startDateTime = new \DateTime($activitystart);
+                $endDateTime = new \DateTime($activityend);
+
+                // Create the result array
+                $result = [];
+
+                if ($startDateTime->format('Y-m-d') === $endDateTime->format('Y-m-d')) {
+                    // Single-day event
+                    $result[] = [
+                        "start" => $startDateTime->format('Y-m-d H:i'),
+                        "end" => $endDateTime->format('Y-m-d H:i')
+                    ];
+                } else {
+                    // Multiday event
+                    $currentDate = clone $startDateTime;
+
+                    while ($currentDate <= $endDateTime) {
+                        // For the first day, use the actual start time
+                        if ($currentDate == $startDateTime) {
+                            $result[] = [
+                                "start" => $startDateTime->format('Y-m-d H:i'),
+                                "end" => $startDateTime->format('Y-m-d') . ' 23:59'
+                            ];
+                        } else if ($currentDate->format('Y-m-d') == $endDateTime->format('Y-m-d')) {
+                            // For the last day, use the actual end time
+                            $result[] = [
+                                "start" => $endDateTime->format('Y-m-d') . ' 00:00',
+                                "end" => $endDateTime->format('Y-m-d H:i')
+                            ];
+                        } else {
+                            // For middle days, use the whole day
+                            $result[] = [
+                                "start" => $currentDate->format('Y-m-d') . ' 00:00',
+                                "end" => $currentDate->format('Y-m-d') . ' 23:59'
+                            ];
+                        }
+
+                        // Move to the next day
+                        $currentDate->modify('+1 day');
+                    }
                 }
 
-                $params = array(
-                    'fileyear' => $currentterminfo->fileyear,
-                    'filesemester' => $currentterminfo->filesemester,
-                    'classcampus' => $activity->get('campus') == 'senior' ? 'SEN' : 'PRH',
-                    'classcode' => 'EX_' . $activity->get('id'),
-                    'description' => $activity->get('activityname'),
-                    'staffid' => $activity->get('staffincharge'),
-                    'leavingdate' => $activitystart,
-                    'returningdate' => $activityend,
-                );
-                $seqnums = $externalDB->get_records_sql($sql, $params); // Returns staffscheduleseq, subjectclassesseq.
-                $seqnums =  array_pop($seqnums);
-                $this->log("The sequence nums (staffscheduleseq, subjectclassesseq): " . json_encode($seqnums), 2);
+                // For each day of this event, create a class.
+                foreach ($result as $day) {
+                    $activitystart = $day['start'];
+                    $activityend = $day['end'];
+                    // Convert start time to DateTime object
+                    $startDateTime = new \DateTime($activitystart);
+                    // Format the month and day as MMDD
+                    $monthDay = $startDateTime->format('md');
+                    $classcode = 'X' . $activity->get('id') . '_' . $monthDay;
 
-                // 2. Insert the extra staff.
-                $extrastaff = $DB->get_records('excursions_staff', array('activityid' => $activity->get('id')));
-                foreach ($extrastaff as $e) {
-                    $this->log("Inserting extra class teacher: " . $e->username, 2);
-                    $sql = $config->insertclassstaffsql . ' :fileyear, :filesemester, :classcampus, :classcode, :staffid';
+                    // 1. Create the class.
+                    $this->log("Creating the class " . $classcode . ", with staff in charge " .  $activity->get('staffincharge') . ", start time " .  $activitystart, 2 );
+                    $sql = $config->createclasssql . ' :fileyear, :filesemester, :classcampus, :classcode, :description, :staffid, :leavingdate, :returningdate';
+                    
+                    // Keep within schedule limits.
+                    $activitystarthour = date('H', $activity->get('timestart'));
+                    if ($activitystarthour < 6) {
+                        $activitystart = date('Y-m-d 06:i', $activity->get('timestart'));
+                    }
+                    if ($activitystarthour > 18 ) {
+                        $activitystart = date('Y-m-d 18:i', $activity->get('timestart'));
+                    }
+
+                    $params = array(
+                        'fileyear' => $currentterminfo->fileyear,
+                        'filesemester' => $currentterminfo->filesemester,
+                        'classcampus' => $activity->get('campus') == 'senior' ? 'SEN' : 'PRH',
+                        'classcode' => $classcode,
+                        'description' => $activity->get('activityname'),
+                        'staffid' => $activity->get('staffincharge'),
+                        'leavingdate' => $activitystart,
+                        'returningdate' => $activityend,
+                    );
+                    $seqnums = $externalDB->get_records_sql($sql, $params); // Returns staffscheduleseq, subjectclassesseq.
+                    $seqnums =  array_pop($seqnums);
+                    $this->log("The sequence nums (staffscheduleseq, subjectclassesseq): " . json_encode($seqnums), 2);
+
+                    // 2. Insert the extra staff.
+                    $extrastaff = $DB->get_records('excursions_staff', array('activityid' => $activity->get('id')));
+                    foreach ($extrastaff as $e) {
+                        $this->log("Inserting extra class teacher: " . $e->username, 2);
+                        $sql = $config->insertclassstaffsql . ' :fileyear, :filesemester, :classcampus, :classcode, :staffid';
+                        $params = array(
+                            'fileyear' => $currentterminfo->fileyear,
+                            'filesemester' => $currentterminfo->filesemester,
+                            'classcampus' => $activity->get('campus') == 'senior' ? 'SEN' : 'PRI',
+                            'classcode' => $classcode,
+                            'staffid' => $e->username,
+                        );
+                        $externalDB->execute($sql, $params);
+                    }
+
+                    // 3. Insert the attending students.
+                    foreach ($attending as $student) {
+                        $this->log("Inserting class student: {$student}.", 2);
+                        $sql = $config->insertclassstudentsql . ' :staffscheduleseq, :fileyear, :filesemester, :classcampus, :classcode, :studentid, :subjectclassesseq';
+                        $params = array(
+                            'staffscheduleseq' => $seqnums->staffscheduleseq,
+                            'fileyear' => $currentterminfo->fileyear,
+                            'filesemester' => $currentterminfo->filesemester,
+                            'classcampus' => $activity->get('campus') == 'senior' ? 'SEN' : 'PRI',
+                            'classcode' => $classcode,
+                            'studentid' => $student,
+                            'subjectclassesseq' => $seqnums->subjectclassesseq,
+                        );
+                        $externalDB->execute($sql, $params);
+                    }
+
+                    // 4. Remove students no longer attending. 
+                    $studentscsv = implode(',', $attending);
+                    $this->log("Delete class students not in list: " . $studentscsv, 2);
+                    $sql = $config->deleteclassstudentssql . ' :fileyear, :filesemester, :classcampus, :classcode, :studentscsv';
                     $params = array(
                         'fileyear' => $currentterminfo->fileyear,
                         'filesemester' => $currentterminfo->filesemester,
                         'classcampus' => $activity->get('campus') == 'senior' ? 'SEN' : 'PRI',
-                        'classcode' => 'EX_' . $activity->get('id'),
-                        'staffid' => $e->username,
+                        'classcode' => $classcode,
+                        'studentscsv' => implode(',', $attending),
                     );
                     $externalDB->execute($sql, $params);
                 }
-
-                // 3. Insert the attending students.
-                foreach ($attending as $student) {
-                    $this->log("Inserting class student: {$student}.", 2);
-                    $sql = $config->insertclassstudentsql . ' :staffscheduleseq, :fileyear, :filesemester, :classcampus, :classcode, :studentid, :subjectclassesseq';
-                    $params = array(
-                        'staffscheduleseq' => $seqnums->staffscheduleseq,
-                        'fileyear' => $currentterminfo->fileyear,
-                        'filesemester' => $currentterminfo->filesemester,
-                        'classcampus' => $activity->get('campus') == 'senior' ? 'SEN' : 'PRI',
-                        'classcode' => 'EX_' . $activity->get('id'),
-                        'studentid' => $student,
-                        'subjectclassesseq' => $seqnums->subjectclassesseq,
-                    );
-                    $externalDB->execute($sql, $params);
-                }
-
-                // 4. Remove students no longer attending. 
-                $studentscsv = implode(',', $attending);
-                $this->log("Delete class students not in list: " . $studentscsv, 2);
-                $sql = $config->deleteclassstudentssql . ' :fileyear, :filesemester, :classcampus, :classcode, :studentscsv';
-                $params = array(
-                    'fileyear' => $currentterminfo->fileyear,
-                    'filesemester' => $currentterminfo->filesemester,
-                    'classcampus' => $activity->get('campus') == 'senior' ? 'SEN' : 'PRI',
-                    'classcode' => 'EX_' . $activity->get('id'),
-                    'studentscsv' => implode(',', $attending),
-                );
-                $externalDB->execute($sql, $params);
 
                 // Mark as processed.
                 $activity->set('classrollprocessed', 1);
